7c95cfc5a830caaa0375806832042170e1998eb2
satyamkumarsingh1 <SATYAMKUMARSINGH6465@GMAIL.COM>
2026-09-03 23:10:26 +0000

    update javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 13456
+let accountemail= "satyam@google.com"
+var accountpassword = "12345"
+acoountCity = "Jaipur"
+ 
+// accountid = 156 // not allowed 
+accountemail = "sat@google.com"
+accountpassword = "4555"
+acoountCity = "Delhi"
+console.log(accountId)
+console.table([ accountemail,accountpassword,acoountCity ])
