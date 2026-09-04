6e6674673f5cf24a694b14a20570ea61bebbf0e8
satyamkumarsingh1 <SATYAMKUMARSINGH6465@GMAIL.COM>
2026-09-04 02:14:40 +0000

    datatypes summary

---
+++ 01_basics/datatypes-Summary.js
@@ -0,0 +1,41 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myobj = {
+    name: "satyam",
+    age: 18,
+
+}
+console.log(myobj)
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+const myFunction2 = function () {
+    console.log("hey there!")
+}
